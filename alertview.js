dea17dccc01aa6db713be3a080c3c8b35425f1f7
Dan Sterrett <dansterrett@gmail.com>
2012-06-22 09:33:06 -0400

    Changed the backdrop to fade in and out.  Added a scale transorm to make the popup expand and collapse similar to an iOS popup.

--- alertview.js
+++ alertview.js
@@ -1,25 +1,41 @@
 'use strict';
 
-var AlertView = {
-  show: function(title, message, primaryButton, secondaryButton, buttonHandler) {
-    var $alertViewBackdrop = $('<div class="alert-view-backdrop"/>');
-    var $alertViewElement = $('<div class="alert-view"/>').appendTo($alertViewBackdrop);
-    var $titleElement = $('<h1>' + title + '</h1>').appendTo($alertViewElement);
-    var $messageElement = $('<p>' + message + '</p>').appendTo($alertViewElement);
 
-    if (secondaryButton) $('<a data-button-index="1" href="#">' + secondaryButton + '</a>').appendTo($alertViewElement);
-    $('<a class="alert-button-primary' + (!secondaryButton ? ' alert-button-single' : '') + '" data-button-index="0" href="#">' + primaryButton + '</a>').appendTo($alertViewElement);
+$(function() {
+  var $alertViewBackdrop = $('<div class="alert-view-backdrop"/>');
+  $(document.body).append($alertViewBackdrop);
+  
+  window.AlertView = {
+    show: function(title, message, primaryButton, secondaryButton, buttonHandler) {
+      var $alertViewElement = $('<div class="alert-view"/>');
+      var $titleElement = $('<h1>' + title + '</h1>').appendTo($alertViewElement);
+      var $messageElement = $('<p>' + message + '</p>').appendTo($alertViewElement);
+
+      if (secondaryButton) $('<a data-button-index="1" href="#">' + secondaryButton + '</a>').appendTo($alertViewElement);
+      $('<a class="alert-button-primary' + (!secondaryButton ? ' alert-button-single' : '') + '" data-button-index="0" href="#">' + primaryButton + '</a>').appendTo($alertViewElement);
     
-    $(document.body).append($alertViewBackdrop);
-    
-    $alertViewElement.delegate('a', 'click', function(evt) {
-      evt.preventDefault();
+      // Add the alertViewElement just before the backdrop
+      $alertViewBackdrop.before($alertViewElement);
+      // Listen for the transition end event
+      var i = 2;
+      $alertViewElement.bind('webkitTransitionEnd transitionend MSTransitionEnd oTransitionEnd transitionEnd', function() {
+        $alertViewElement.addClass('av-zoom-' + i++);
+      });
       
-      if (buttonHandler && typeof buttonHandler === 'function') {
-        buttonHandler(parseInt($(this).attr('data-button-index'), 10));
-      }
+      // Call offset() to force a redraw before adding the av-zoom class
+      $alertViewElement.offset();
+      $alertViewElement.addClass('av-zoom-1');
       
-      $alertViewBackdrop.remove();
-    });
-  }
-};
+      $alertViewElement.delegate('a', 'click', function(evt) {
+        evt.preventDefault();
+      
+        if (buttonHandler && typeof buttonHandler === 'function') {
+          buttonHandler(parseInt($(this).attr('data-button-index'), 10));
+        }
+      
+        $alertViewElement.remove();
+      });
+    }
+  };
+
+});
