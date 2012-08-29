4e4184e793d155f0929f91e3b38523f8fa6d28b5
Dan Sterrett <dansterrett@gmail.com>
2012-08-29 15:03:08 -0400

    Added ability to have up to three buttons.

--- alertview.js
+++ alertview.js
@@ -5,27 +5,45 @@ $(function() {
   var $alertViewBackdrop = $('<div class="alert-view-backdrop"/>');
   $(document.body).append($alertViewBackdrop);
   
+  /**
+    Shows the AlertView modal.
+    @param {Object} Optional properties: title, message, buttons, and callback.
+  */
   window.AlertView = {
-    show: function(title, message, primaryButton, secondaryButton, buttonHandler) {
-      var $alertViewElement = $('<div class="alert-view animated bounceIn"/>');
-      var $titleElement = $('<h1>' + title + '</h1>').appendTo($alertViewElement);
-      var $messageElement = $('<p>' + message + '</p>').appendTo($alertViewElement);
+    show: function(settings) {
+      var title = settings.title;
+      var message = settings.message;
+      var buttons = settings.buttons;
+      var callback = settings.callback;
 
-      if (secondaryButton) $('<a data-button-index="1" href="#">' + secondaryButton + '</a>').appendTo($alertViewElement);
-      $('<a class="alert-button-primary' + (!secondaryButton ? ' alert-button-single' : '') + '" data-button-index="0" href="#">' + primaryButton + '</a>').appendTo($alertViewElement);
-    
+      var $alertViewElement = $('<div class="alert-view animated bounceIn"/>');
+      
+      if (title) $('<h1>' + title + '</h1>').appendTo($alertViewElement);
+      
+      if (message) $('<p>' + message + '</p>').appendTo($alertViewElement);
+      
+      if (buttons) {
+        var numberOfButtons = buttons.length;
+        if (numberOfButtons > 0) {
+          $alertViewElement.addClass('button-count-' + numberOfButtons);
+          for (var i = 0; i < numberOfButtons; i++) {
+            $('<a ' + (i === 0 ? 'class="alert-button-primary" ' : '') + 'data-button-index="' + i + '" href="#">' + buttons[i] + '</a>').appendTo($alertViewElement);
+          }
+        }
+      }
+
+      if (callback && typeof callback === 'function') {
+        $alertViewElement.delegate('a', 'click', function(evt) {
+          evt.preventDefault();
+        
+          callback(parseInt($(this).attr('data-button-index'), 10));
+        
+          $alertViewElement.remove();
+        });
+      }
+
       // Add the alertViewElement just before the backdrop
       $alertViewBackdrop.before($alertViewElement);
-      
-      $alertViewElement.delegate('a', 'click', function(evt) {
-        evt.preventDefault();
-      
-        if (buttonHandler && typeof buttonHandler === 'function') {
-          buttonHandler(parseInt($(this).attr('data-button-index'), 10));
-        }
-      
-        $alertViewElement.remove();
-      });
     }
   };
 
